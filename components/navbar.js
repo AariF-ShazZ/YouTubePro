8813554fad4a39f4fcfadecb10c737a4e5370b5f
Aarif-shazZ <shazaarif0@gmail.com>
2023-01-24 19:43:28 +0530

    done

---
+++ components/navbar.js
@@ -0,0 +1,45 @@
+// function navbar() {
+//     return `
+//     <div class="nav-laft flex-div">
+//     <img src="images/images/menu.png" alt="" class="menu-icon">
+//     <img src="images/images/download.png" alt="" class="logo">
+// </div>
+// <div class="nav-middle flex-div">
+//     <div class="search-box flex-div">
+//         <input type="text"  placeholder="search">
+//         <img src="Images/images/search.png" alt="">
+//     </div>
+//     <img src="Images/images/voice-search.png" alt="" class="mic-icon">
+// </div>
+// <div class="nav-right flex-div">
+//     <img src="images/images/upload.png" alt="">
+//     <img src="images/images/more.png" alt="">
+//     <img src="images/images/notification.png" alt="">
+//     <img src="images/images/Jack.png" alt="" class="user-icon">
+// </div>
+//     `;
+// }
+// export default navbar
+
+
+// let navbar=()=>{
+//     return `
+//     <div class="nav-laft flex-div">
+//     <img src="images/images/menu.png" alt="" class="menu-icon">
+//     <img src="images/images/download.png" alt="" class="logo">
+// </div>
+// <div class="nav-middle flex-div">
+//     <div class="search-box flex-div">
+//         <input type="text"  placeholder="search">
+//         <img src="Images/images/search.png" alt="">
+//     </div>
+//     <img src="Images/images/voice-search.png" alt="" class="mic-icon">
+// </div>
+// <div class="nav-right flex-div">
+//     <img src="images/images/upload.png" alt="">
+//     <img src="images/images/more.png" alt="">
+//     <img src="images/images/notification.png" alt="">
+//     <img src="images/images/Jack.png" alt="" class="user-icon">
+// </div>`
+// }
+// export default navbar;
---
+++ components/sidebar.js
@@ -0,0 +1,51 @@
+let sidebar  = () => {
+    return `
+    <div class="shortcut-links">
+    <a href=""><img src="Images/images/home.png" alt="">
+        <p>Home</p>
+    </a>
+    <a href=""><img src="Images/images/explore.png" alt="">
+        <p>Explore</p>
+    </a>
+    <a href=""><img src="Images/images/subscriprion.png" alt="">
+        <p>Subscriprion</p>
+    </a>
+    <a href=""><img src="Images/images/library.png" alt="">
+        <p>Library</p>
+    </a>
+    <a href=""><img src="Images/images/history.png" alt="">
+        <p>History</p>
+    </a>
+    <a href=""><img src="Images/images/playlist.png" alt="">
+        <p>Playlist</p>
+    </a>
+    <a href=""><img src="Images/images/messages.png" alt="">
+        <p>Messages</p>
+    </a>
+    <a href=""><img src="Images/images/show-more.png" alt="">
+        <p>Show More</p>
+    </a>
+    <hr>
+</div>
+<div class="subscribed-list">
+    <h3>SUBSCRIBED</h3>
+    <a href=""><img src="Images/images/Jack.png" alt="">
+        <p>Jack Nicholson</p>
+    </a>
+    <a href=""><img src="Images/images/simon.png" alt="">
+        <p>Simon Nicholson</p>
+    </a>
+    <a href=""><img src="Images/images/tom.png" alt="">
+        <p>Aarif Nicholson</p>
+    </a>
+    <a href=""><img src="Images/images/megan.png" alt="">
+        <p>Imran Nicholson</p>
+    </a>
+    <a href=""><img src="Images/images/cameron.png" alt="">
+        <p>Salman Khan </p>
+    </a>
+</div>
+    `
+}
+
+export default sidebar
