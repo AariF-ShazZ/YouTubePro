4c1aaad5f7f03416dd6e4d24ff404cc6779dd4ea
Aarif-shazZ <shazaarif0@gmail.com>
2023-11-11 12:28:36 +0530

    done

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// let api_key = "AIzaSyCmLhASSz82amuby1nky44Eu6Hw042i630"
+// let video_http = "https://www.googleapis.com/youtube/v3/videos?"
+
 var menuIcon = document.querySelector(".menu-icon")
 var sidebar = document.querySelector(".sidebar")
 var container = document.querySelector(".container")
